ed24688278cc1ec021a1a9f07db417183cfa7c93
bens8235 <bmshreeve3@gmail.com>
2024-02-02 19:45:49 +0000

    submission

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,13 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Football Debate</h1>
-      <Link href="/content">
-        <h2>Table Football</h2>
-      </Link>
-    </div>
+    <>
+      <div>
+        <h1>Football Debate</h1>
+        <Link href="/content">
+          <h2>Table Football</h2>
+        </Link>
+      </div>
+    </>
   );
 }
